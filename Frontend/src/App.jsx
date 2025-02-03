4ffbb0698b60b6211cbf5830c93e90410c3d31d5
shrutikamani <shrutikamani2003@gmail.co>
2025-02-03 13:55:22 +0530

    shruti add

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Shruti from './Shruti'
+
+const App = () =>
+{
+  return (
+    <>
+      <Shruti />
+    </>
+
+  )
+}
+
+export default App
---
+++ Frontend/src/Shruti.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Shruti = () =>
+{
+  return (
+    <div>i m Shruti</div>
+  )
+}
+
+export default Shruti
